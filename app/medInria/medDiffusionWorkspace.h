2ab6d074c5eca2bfed73777ceb5f4913082f9b58
Olivier Commowick <olivier.commowick@inria.fr>
2014-03-10 11:09:40 +0100

    Started work on diffusion after refactor

--- app/medInria/medDiffusionWorkspace.h
+++ app/medInria/medDiffusionWorkspace.h
@@ -51,14 +51,7 @@ public slots:
     void connectCurrentViewSignals(dtkAbstractView *view);
     void disconnectCurrentViewSignals(dtkAbstractView *view);
 
-    /**
-* @brief Adds a new tab to a workspace
-*
-* Re-implemented, replaces default implementation in medAbstractWorkspace
-*/
-    void onAddTabClicked();
-
-    void changeCurrentContainer(QString name);
+    void changeCurrentContainer();
 
 private:
     medDiffusionWorkspacePrivate *d;
